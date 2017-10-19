01bb39f830c7d73e9ee688009ad2d64b0e519330
kutmicro2017 <32184745+kutmicro2017@users.noreply.github.com>
2017-10-19 19:10:07 +0900

    Update 2016136110.ino

--- 2016136110.ino
+++ 2016136110.ino
@@ -11,7 +11,7 @@
 #define Ri_Y 23
 #define Ri_G 24 
 #define Button_A 2 // 스위치
-int delaytime = 10000; //40초
+int delaytime = 40000; //40초
 volatile bool state = true; //스위치 변수, 누를 시 false
 
 void setup() {  // OUTPUT 세팅
@@ -37,63 +37,45 @@ void loop() {
 // -----------------------------------------------------
 
 
-//------------맨처음, 상 신호등만 초록색 ----------------  
+//------------맨처음, 상 하 신호등만 초록색 ----------------  
 digitalWrite(Le_R, HIGH);
 digitalWrite(Ri_R, HIGH);
-digitalWrite(Do_R, HIGH);
+digitalWrite(Do_R, LOW);
 digitalWrite(Up_R, LOW);
 digitalWrite(Up_G, HIGH);
-delay(delaytime); //40초 동안 유지
-//------------------------------------------------------.
-digitalWrite(Up_G, LOW); // 상 신호등 초록색 꺼짐
-
-// -------------초록색 -> 빨간색 될때 ------------------------
-Yellow(Up_Y);
-// --------------------------------------------------------
-// ---------------------하 신호등 빨간불이 초록불로 바뀜------------------------
-digitalWrite(Up_R, HIGH); 
-digitalWrite(Do_R, LOW);
 digitalWrite(Do_G, HIGH); 
-// ------------------------------------------------------------------------------
-
-for(int count=0; count<1000;count++) { // 앞으로 10초동안 반복
- if(state == false) { //스위치가 눌렸으면
-    delay(1000); //3초 대기
-    digitalWrite(Do_G, LOW); // 하 신호등 초록불을 꺼준다
-    Yellow(Do_Y); // 하 신호등 노란불 점멸
-    digitalWrite(Do_R, HIGH); // 하 신호등 빨간불을 켠다
-    state = true; // 상태를 처음의 ture 로 바꿔줌
- }
- if(count == (delaytime/1000)) break;
- delay(1000); // 무조건 적으로 10초 기다릴것
+for(int count=0;count<40;count++){ //40초간 초록불 유지
+  if(state == false){ //스위치가 눌리면
+    delay(1000); //1초 후
+    digitalWrite(Up_G, LOW);
+    digitalWrite(Do_G, LOW);
+    Yellow(Up_Y, Do_Y);
+    digitalWrite(Up_R, HIGH);
+    digitalWrite(Do_R, HIGH); //점멸 후 빨간색
+    state = true;
+    if(count>=30){ //30초 이상이 지나있을 경우 for문 빠져나옴
+      break; 
+    }
+    else{ //30초 이하라면 다시 상하 켜줌
+      digitalWrite(Up_R, LOW);
+      digitalWrite(Do_R, LOW);
+      digitalWrite(Up_G, HIGH);
+      digitalWrite(Do_G, HIGH);
+      count += 10; //10초가 지난만큼 추가
+    }
+  }
+  delay(1000);
 }
 
-
-if(digitalRead(Do_G) == HIGH) { //스위치가 눌리지 않았을 경우 진행
-    digitalWrite(Do_G, LOW);  // 하 초록불 꺼짐
-    Yellow(Do_Y); // 하 노란불 점멸
-    digitalWrite(Do_R, HIGH); // 하 빨간불 켜짐
-}
-
-
-
-digitalWrite(Do_R, HIGH);  // 하 빨간불 켜져있음
-digitalWrite(Le_R, LOW); // 좌 빨간불 꺼짐
-digitalWrite(Le_G, HIGH); // 좌 신호등 초록불만 켜짐
-delay(delaytime); //40초간 유지
-digitalWrite(Le_G, LOW); //좌 초록불 꺼짐
-Yellow(Le_Y); //좌 노란불 점멸
-digitalWrite(Le_R, HIGH); // 좌 빨간불 켜짐
-
-
-digitalWrite(Ri_R, LOW); // 우 빨간불 꺼짐
-digitalWrite(Ri_G, HIGH);  // 우 초록불 켜짐
-delay(delaytime); 
-digitalWrite(Ri_G, LOW); // 우 초록불 꺼짐
-Yellow(Ri_Y); // 우 노란불 점멸
-digitalWrite(Ri_G, LOW); // 우 초록불 꺼짐
-digitalWrite(Ri_R, HIGH); // 우 빨간불 켜짐
-
+digitalWrite(Le_R, LOW);
+digitalWrite(Ri_R, LOW);
+digitalWrite(Le_G, HIGH);
+digitalWrite(Ri_G, HIGH);
+digitalWrite(Do_R, HIGH);
+digitalWrite(Up_R, HIGH);
+digitalWrite(Up_G, LOW);
+digitalWrite(Do_G, LOW);
+delay(delaytime); //40초간 좌우 초록불 온
 }
 
 
@@ -108,17 +90,19 @@ digitalWrite(Ri_R, HIGH); // 우 빨간불 켜짐
 
 //----------------------------------함수들--------------------------------
 void Botton(){ //스위치가 눌렸을 때 실행되는 함수
- if(digitalRead(Do_G) == HIGH) state = false; //스위치가 눌리면 state의 값을 바꿔준다
+ if(digitalRead(Do_G) == HIGH && digitalRead(Up_G) == HIGH) state = false; //스위치가 눌리면 state의 값을 바꿔준다
 }
 
 
 
 
-void Yellow(const int LED) {  //yellow신호 점멸
+void Yellow(const int LED1, const int LED2) {  //yellow신호 점멸
    for(int i =0; i < 10; i++) {
-    digitalWrite(LED, HIGH);
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, HIGH);
     delay(500);
-    digitalWrite(LED, LOW);
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
     delay(500);
    }
 }
